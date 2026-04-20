b6e98153379f7d93dbaeccde7e0c68afcbf9fd66
jasmin varvadiya <jasminvarvadiya@gmail.com>
2026-04-20 13:55:20 +0530

    text- w-full

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -7,7 +7,7 @@ import "locomotive-scroll/dist/locomotive-scroll.css";
 function Marquee() {
   return (
     <>
-      <div data-scroll data-scroll-section='.2'  className="container z-100 relative pt-5 w-full ">
+      <div data-scroll data-scroll-section='.2'  className="container z-[100] relative pt-5 w-full ">
         <div className="content w-full">
           <div className="w-full rounded-tl-3xl rounded-tr-3xl   py-15 bg-[#004d43] overflow-hidden">
             <div className="flex border-t-2 border-b-2 border-white">
@@ -17,23 +17,23 @@ function Marquee() {
                 transition={{ ease: "linear", repeat: Infinity, duration: 25 }}
                 className="flex gap-10 whitespace-nowrap font-['font-2']"
               >
-                <h1 className="text-[23vw] text-white font-bold leading-none">
+                <h1 className="text-[23vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
-                <h1 className="text-[22vw] text-white font-bold leading-none">
+                <h1 className="text-[22vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
-                <h1 className="text-[22vw] text-white font-bold leading-none">
+                <h1 className="text-[22vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
 
-                <h1 className="text-[22vw] text-white font-bold leading-none">
+                <h1 className="text-[22vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
-                <h1 className="text-[22vw] text-white font-bold leading-none">
+                <h1 className="text-[22vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
-                <h1 className="text-[22vw] text-white font-bold leading-none">
+                <h1 className="text-[22vw] text-[20vw] md:text-[18vw] lg:text-[16vw] text-white font-bold leading-none">
                   WE ARE OCHI
                 </h1>
               </motion.div>
